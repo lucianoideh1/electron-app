07ffd708743987f2a6f11012769ac834cc03832e
lucianoideh1 <vazquezluciano5@gmail.com>
2023-03-06 16:36:44 -0300

    created a window with js, that runs a web

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("Hola desde electron");
+const {app, BrowserWindow} = require("electron")
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width:800,
+        height:800
+    })
+
+    window.loadFile("index.html")
+}
+
+app.whenReady().then(() =>{
+    createWindow()
+})
